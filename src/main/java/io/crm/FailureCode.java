c5565c49e0933aeb79ff318f4382b0eb9d9fc73c
codefacts <codefacts63@gmail.com>
2015-08-23 17:05:48 +0600

    io.crm:common:1.0-SNAPSHOT

--- src/main/java/io/crm/FailureCode.java
+++ src/main/java/io/crm/FailureCode.java
@@ -4,7 +4,8 @@ package io.crm;
  * Created by someone on 18/08/2015.
  */
 public enum FailureCode {
-    validationError(1, "Validation Error"), InternalServerError(2, "Internal Server Error");
+    validationError(1, "Validation Error"),
+    InternalServerError(2, "Internal Server Error");
 
     public final int code;
     public final String message;
