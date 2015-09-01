abbe556cec6380d90d3efdf2bfebbb4fe9eebaf3
codefacts <codefacts63@gmail.com>
2015-09-01 22:39:00 +0600

    CampaignConfigureTree

---
+++ src/main/java/io/crm/Events.java
@@ -0,0 +1,70 @@
+package io.crm;
+
+/**
+ * Created by someone on 01/09/2015.
+ */
+final public class Events {
+    public static final String CREATE_NEW_ADMIN = "create-new-admin";
+    public static final String CREATE_NEW_HEAD_OFFICE = "create-new-head-office";
+
+    public static final String CREATE_NEW_BR = "CREATE_NEW_BR";
+    public static final String CREATE_NEW_AREA_COORDINATOR = "CREATE_NEW_AREA_COORDINATOR";
+    public static final String CREATE_NEW_BR_SUPERVISOR = "CREATE_NEW_BR_SUPERVISOR";
+
+    public static final String NEW_ADMIN_CREATED = "new-admin-created";
+    public static final String NEW_HEAD_OFFICE_CREATED = "new-head-office-created";
+    public static final String NEW_BR_CREATED = "NEW_BR_CREATED";
+    public static final String NEW_BR_SUPERVISOR_CREATED = "NEW_BR_SUPERVISOR_CREATED";
+    public static final String NEW_AREA_COORDINATOR_CREATED = "NEW_AREA_COORDINATOR_CREATED";
+
+    public static final String FIND_ALL_REGIONS = "FIND_ALL_REGIONS";
+    public static final String FIND_ALL_AREAS = "FIND_ALL_AREAS";
+    public static final String FIND_ALL_HOUSES = "FIND_ALL_HOUSES";
+    public static final String FIND_ALL_LOCATIONS = "FIND_ALL_LOCATIONS";
+    public static final String FIND_ALL_BRANDS = "FIND_ALL_BRANDS";
+    public static final String FIND_ALL_EMPLOYEES = "FIND_ALL_EMPLOYEES";
+    public static final String FIND_ALL_CLIENTS = "FIND_ALL_CLIENTS";
+    public static final String FIND_ALL_CONTACTS = "FIND_ALL_CONTACTS";
+
+    public static final String GET_DB_TREE = "GET_DB_TREE";
+    public static final String GET_COLLECTION_COUNT = "GET_COLLECTION_COUNT";
+
+    public static final String UPDATE_EMPLOYEE = "UPDATE_EMPLOYEE";
+    public static final String CREATE_EMPLOYEE = "CREATE_EMPLOYEE";
+    public static final String FIND_ALL_USER_TYPES = "FIND_ALL_USER_TYPES";
+    public static final String FIND_EMPLOYEE = "FIND_EMPLOYEE";
+    public static final String FIND_ALL_CAMPAIGNS = "FIND_ALL_CAMPAIGNS";
+    public static final String GET_DB_TREE_WITH_USERS = "GET_DB_TREE_WITH_USERS";
+
+
+    public static final String FIND_ALL_TOWNS = "FIND_ALL_TOWNS";
+    public static final String NEW_AREA_CREATED = "NEW_AREA_CREATED";
+
+    public static final String CREATE_AREA = "CREATE_AREA";
+    public static final String UPDATE_AREA = "UPDATE_AREA";
+    public static final String AREA_UPDATED = "AREA_UPDATED";
+    public static final String UPDATE_REGION = "UPDATE_REGION";
+    public static final String NEW_REGION_CREATED = "NEW_REGION_CREATED";
+    public static final String REGION_UPDATED = "REGION_UPDATED";
+    public static final String CREATE_REGION = "CREATE_REGION";
+    public static final String NEW_HOUSE_CREATED = "NEW_HOUSE_CREATED";
+    public static final String HOUSE_UPDATED = "HOUSE_UPDATED";
+    public static final String UPDATE_HOUSE = "UPDATE_HOUSE";
+    public static final String CREATE_HOUSE = "CREATE_HOUSE";
+    public static final String NEW_LOCATION_CREATED = "NEW_LOCATION_CREATED";
+    public static final String LOCATION_UPDATED = "LOCATION_UPDATED";
+    public static final String UPDATE_LOCATION = "UPDATE_LOCATION";
+    public static final String CREATE_LOCATION = "CREATE_LOCATION";
+    public static final String NEW_BRAND_CREATED = "NEW_BRAND_CREATED";
+    public static final String BRAND_UPDATED = "BRAND_UPDATED";
+    public static final String UPDATE_BRAND = "UPDATE_BRAND";
+    public static final String CREATE_BRAND = "CREATE_BRAND";
+    public static final String UPDATE_USER_TYPE = "UPDATE_USER_TYPE";
+    public static final String CREATE_USER_TYPE = "CREATE_USER_TYPE";
+    public static final String NEW_USER_TYPE_CREATED = "NEW_USER_TYPE_CREATED";
+    public static final String USER_TYPE_UPDATED = "USER_TYPE_UPDATED";
+    public static final String NEW_CAMPAIGN_CREATED = "NEW_CAMPAIGN_CREATED";
+    public static final String CAMPAIGN_UPDATED = "CAMPAIGN_UPDATED";
+    public static final String CREATE_CAMPAIGN = "CREATE_CAMPAIGN";
+    public static final String UPDATE_CAMPAIGN = "UPDATE_CAMPAIGN";
+}
