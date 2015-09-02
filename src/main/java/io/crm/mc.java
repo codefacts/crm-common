ba6605c69fe16a4a8b100d5a77ac7ff615256bf9
codefacts <codefacts63@gmail.com>
2015-09-02 13:50:40 +0600

    All Module Starter Verticle

--- src/main/java/io/crm/mc.java
+++ src/main/java/io/crm/mc.java
@@ -1,5 +1,7 @@
 package io.crm;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Created by sohan on 7/29/2015.
  */
@@ -20,19 +22,19 @@ public enum mc {
     public final String label;
     public final String fieldName;
     private final boolean internal;
-    private long nextId = 0;
+    private AtomicLong nextId = new AtomicLong(0);
 
     mc(String fieldName, String label, long nextId, boolean internal) {
         this.fieldName = fieldName;
         this.label = label;
-        this.nextId = nextId;
+        this.nextId.set(nextId);
         this.internal = internal;
     }
 
     mc(String fieldName, String label, long nextId) {
         this.fieldName = fieldName;
         this.label = label;
-        this.nextId = nextId;
+        this.nextId.set(nextId);
         this.internal = false;
     }
 
@@ -46,31 +48,32 @@ public enum mc {
         return internal;
     }
 
-    public void setNextId(long nextId) {
-        this.nextId = nextId;
+    public void setNextId(final long nextId) {
+        this.nextId.set(nextId);
+        ;
     }
 
     public long getNextId() {
-        return nextId;
+        return nextId.get();
     }
 
     public long incrementNextId() {
-        return nextId++;
+        return nextId.incrementAndGet();
     }
 
     public long decrementNextId() {
-        return nextId--;
+        return nextId.decrementAndGet();
     }
 
-    public long decrementNextId(long amount) {
-        return nextId -= amount;
+    public long decrementNextId(final long amount) {
+        return nextId.addAndGet(-amount);
     }
 
-    public long incrementNextId(long amount) {
-        return nextId += amount;
+    public long incrementNextId(final long amount) {
+        return nextId.addAndGet(amount);
     }
 
     public boolean isIdTypeLong() {
-        return nextId >= 0;
+        return nextId.get() >= 0;
     }
 }
