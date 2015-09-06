7fcee9b26095a9e4fa342a49e7eccd19ff738dd3
codefacts <codefacts63@gmail.com>
2015-09-06 13:12:46 +0600

    Campaign Tree

---
+++ src/main/java/io/crm/util/Touple2Boolean.java
@@ -0,0 +1,14 @@
+package io.crm.util;
+
+/**
+ * Created by someone on 04/09/2015.
+ */
+final public class Touple2Boolean {
+    public final boolean t1;
+    public final boolean t2;
+
+    public Touple2Boolean(boolean t1, boolean t2) {
+        this.t1 = t1;
+        this.t2 = t2;
+    }
+}
