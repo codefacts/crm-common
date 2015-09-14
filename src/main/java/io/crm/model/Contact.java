d45428a1cc07645acf53b59629e447d6fa4ae844
codefacts <codefacts63@gmail.com>
2015-09-14 12:45:32 +0600

    Json-Schema-validation and Move model package to common

--- src/main/java/io/crm/model/Contact.java
+++ src/main/java/io/crm/model/Contact.java
@@ -6,20 +6,9 @@ import java.io.Serializable;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Contact implements Serializable {
-    public static final String campaign = "campaign";
-    public static final String region = "region";
-    public static final String area = "area";
-    public static final String distributionHouse = "distributionHouse";
     public static final String br = "br";
     public static final String consumer = "consumer";
     public static final String brand = "brand";
-    public static final String name = "name";
-    public static final String fatherName = "fatherName";
-    public static final String mobile = "mobile";
-    public static final String occupation = "occupation";
-
-    public static final String age = "age";
-    public static final String date = "date";
 
     public static final String description = "description";
 
@@ -29,7 +18,6 @@ public class Contact implements Serializable {
     public static final String latitude = "latitude";
     public static final String longitude = "longitude";
     public static final String accuracy = "accuracy";
-    public static final String location = "location";
     public static String receive_date = "receive_date";
 
     Contact() {
