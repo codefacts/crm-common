cbac98e0f081cf5e8f22a442d410c2f200bf5b5b
codefacts <codefacts63@gmail.com>
2015-10-17 18:57:17 +0600

    Promises.All

--- src/main/java/io/crm/promise/Promises.java
+++ src/main/java/io/crm/promise/Promises.java
@@ -3,6 +3,7 @@ package io.crm.promise;
 import diag.Watch;
 import io.crm.promise.intfs.Defer;
 import io.crm.promise.intfs.Promise;
+import io.crm.util.Touple2;
 import io.vertx.core.Vertx;
 
 import java.util.concurrent.TimeoutException;
@@ -33,228 +34,24 @@ final public class Promises {
         return promise;
     }
 
-    public static void main(String... args) {
-        Watch watch = new Watch().start();
-        for (int i = 0; i < 50_000_000; i++) {
-            final Defer<Object> defer = defer();
-            defer.promise()
-//            Promises.success(55)
-                    .mapTo(null)
-                    .mapToPromise(null)
-                    .mapToVoid(null)
-                    .success(null)
-                    .complete(null)
-                    .error(null)
-            ;
-            defer.complete(31);
-        }
-        System.out.println(watch.end().elapsed() + " total: " + PromiseImpl.total);
-    }
-
-    public static void test8() {
-        Watch watch = new Watch().start();
-        for (int i = 0; i < 50_000_000; i++) {
-            final Defer<Object> defer = defer();
-            defer.promise()
-//            Promises.success(55)
-                    .success(null)
-                    .error(null)
-                    .mapTo(null)
-                    .mapTo(null)
-                    .success(null)
-                    .mapTo(null)
-                    .complete(null)
-                    .error(null)
-                    .mapToPromise(null)
-                    .complete(null)
-                    .mapTo(null)
-                    .complete(null)
-                    .error(null)
-                    .mapToPromise(null)
-                    .complete(null)
-                    .mapTo(null)
-                    .success(null)
-                    .error(null)
-                    .mapTo(null)
-                    .mapToPromise(null)
-                    .complete(null)
-                    .success(null)
-                    .error(null)
-                    .mapTo(null)
-                    .mapToPromise(null)
-                    .complete(null)
-                    .mapToVoid(null)
-                    .error(null)
-                    .mapTo(null)
-                    .mapToPromise(null)
-                    .complete(null)
-                    .mapToVoid(null)
-                    .success(null)
-                    .error(null)
-                    .mapTo(null)
-                    .success(null)
-                    .error(null)
-                    .success(null)
-                    .error(null)
-                    .mapTo(null)
-                    .mapToPromise(null)
-                    .complete(null)
-                    .mapToVoid(null)
-                    .error(null)
-                    .mapTo(null)
-                    .mapToPromise(null)
-                    .complete(null)
-                    .mapToVoid(null)
-                    .success(null)
-                    .error(null)
-                    .mapTo(null)
-                    .success(null)
-                    .error(null)
-                    .mapToVoid(null)
-                    .error(null)
-                    .mapTo(null)
-                    .mapToPromise(null)
-                    .complete(null)
-                    .mapToVoid(null)
-                    .success(null)
-                    .error(null)
-                    .mapTo(null)
-                    .success(null)
-                    .error(null)
-            ;
-            defer.complete(31);
-        }
-        System.out.println(watch.end().elapsed() + " total: " + PromiseImpl.total);
-    }
-
-    public static void test7() {
-        Watch watch = new Watch().start();
-        for (int i = 0; i < 10_000_000; i++) {
-            final Defer<Object> defer = defer();
-            defer.promise()
-                    .success(null)
-                    .error(null)
-                    .mapTo(s -> s.toString())
-                    .mapTo(s -> s.split("-"))
-                    .success(null)
-                    .mapTo(s -> String.join("-", s))
-                    .complete(null)
-                    .error(null)
-                    .mapToPromise(m -> Promises.success(m.split("5")))
-                    .complete(null)
-                    .mapTo(s -> String.join("-", s))
-                    .complete(null)
-                    .error(null)
-                    .mapToPromise(m -> Promises.success(m.split("5")))
-                    .complete(null)
-                    .mapTo(s -> s.length)
-                    .success(null)
-                    .error(null)
-                    .mapTo(null)
-                    .mapToPromise(null)
-                    .complete(null)
-                    .mapToVoid(null)
-                    .error(null)
-                    .mapTo(null)
-                    .mapToPromise(null)
-                    .complete(null)
-                    .mapToVoid(null)
-                    .success(null)
-                    .error(null)
-                    .mapTo(v -> {
-                        return "[void]";
-                    })
-                    .success(null)
-                    .error(null)
-            ;
-            defer.complete(123);
-        }
-        System.out.println(watch.end().elapsed() + " total: " + PromiseImpl.total);
-    }
-
-    public static void test6() {
-        final Defer<Object> defer = defer();
-        defer.promise()
-                .success(null)
-                .error(null)
-                .success(null)
-                .complete(null)
-                .error(null)
-                .complete(null)
-                .success(null)
-                .error(null)
+    public static <T1, T2> Promise<Touple2<T1, T2>> all(final Promise<T1> t1Promise, final Promise<T2> t2Promise) {
+        final Defer<Touple2<T1, T2>> defer = defer();
+        t1Promise
+                .complete(t1p -> {
+                    if (t1p.isSuccess()) {
+                        t2Promise
+                                .complete(t2p -> {
+                                    if (t2p.isSuccess()) {
+                                        defer.complete(new Touple2<T1, T2>(t1p.get(), t2p.get()));
+                                    } else {
+                                        defer.fail(t2p.error());
+                                    }
+                                });
+                    } else {
+                        defer.fail(t1p.error());
+                    }
+                })
         ;
-        defer.fail(new Exception("NO NO"));
-    }
-
-    public static void test5() {
-        final Defer<Object> defer = defer();
-        defer.promise()
-                .success(null)
-                .error(null)
-                .success(null)
-                .complete(null)
-                .error(null)
-                .complete(null)
-                .success(null)
-                .error(null)
-        ;
-        defer.complete();
-    }
-
-    public static void test4() {
-        final Defer<Object> defer = defer();
-        defer.promise()
-                .success(null)
-                .error(null)
-                .success(null)
-                .complete(null)
-                .error(null)
-                .complete(null)
-                .success(null)
-                .error(null)
-        ;
-        defer.complete("8888888888888888888");
-    }
-
-    public static void test3() {
-        final Defer<Object> defer = defer();
-        defer.promise()
-                .success(null)
-                .error(null)
-                .success(null)
-                .complete(null)
-                .error(null)
-                .complete(null)
-                .success(null)
-                .error(null)
-        ;
-        defer.fail(new TimeoutException("Delay: 55"));
-    }
-
-    private static void test2() {
-        error(new TimeoutException("Timed out: 55"))
-                .success(null)
-                .error(null)
-                .success(null)
-                .complete(null)
-                .error(null)
-                .complete(null)
-                .success(null)
-                .error(null)
-        ;
-    }
-
-    private static void test1() {
-        success("Sona")
-                .success(null)
-                .error(null)
-                .success(null)
-                .complete(null)
-                .error(null)
-                .complete(null)
-                .success(null)
-                .error(null)
-        ;
+        return defer.promise();
     }
 }
---
+++ src/main/java/test/PromiseTest.java
@@ -0,0 +1,240 @@
+package test;
+
+import diag.Watch;
+import io.crm.promise.PromiseImpl;
+import io.crm.promise.Promises;
+import io.crm.promise.intfs.Defer;
+import static io.crm.promise.Promises.*;
+
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by someone on 17/10/2015.
+ */
+public class PromiseTest {
+
+    public static void main(String... args) {
+        Watch watch = new Watch().start();
+        for (int i = 0; i < 50_000_000; i++) {
+            final Defer<Object> defer = defer();
+            defer.promise()
+//            Promises.success(55)
+                    .mapTo(null)
+                    .mapToPromise(null)
+                    .mapToVoid(null)
+                    .success(null)
+                    .complete(null)
+                    .error(null)
+            ;
+            defer.complete(31);
+        }
+        System.out.println(watch.end().elapsed() + " total: " + PromiseImpl.total);
+    }
+
+    public static void test8() {
+        Watch watch = new Watch().start();
+        for (int i = 0; i < 50_000_000; i++) {
+            final Defer<Object> defer = defer();
+            defer.promise()
+//            Promises.success(55)
+                    .success(null)
+                    .error(null)
+                    .mapTo(null)
+                    .mapTo(null)
+                    .success(null)
+                    .mapTo(null)
+                    .complete(null)
+                    .error(null)
+                    .mapToPromise(null)
+                    .complete(null)
+                    .mapTo(null)
+                    .complete(null)
+                    .error(null)
+                    .mapToPromise(null)
+                    .complete(null)
+                    .mapTo(null)
+                    .success(null)
+                    .error(null)
+                    .mapTo(null)
+                    .mapToPromise(null)
+                    .complete(null)
+                    .success(null)
+                    .error(null)
+                    .mapTo(null)
+                    .mapToPromise(null)
+                    .complete(null)
+                    .mapToVoid(null)
+                    .error(null)
+                    .mapTo(null)
+                    .mapToPromise(null)
+                    .complete(null)
+                    .mapToVoid(null)
+                    .success(null)
+                    .error(null)
+                    .mapTo(null)
+                    .success(null)
+                    .error(null)
+                    .success(null)
+                    .error(null)
+                    .mapTo(null)
+                    .mapToPromise(null)
+                    .complete(null)
+                    .mapToVoid(null)
+                    .error(null)
+                    .mapTo(null)
+                    .mapToPromise(null)
+                    .complete(null)
+                    .mapToVoid(null)
+                    .success(null)
+                    .error(null)
+                    .mapTo(null)
+                    .success(null)
+                    .error(null)
+                    .mapToVoid(null)
+                    .error(null)
+                    .mapTo(null)
+                    .mapToPromise(null)
+                    .complete(null)
+                    .mapToVoid(null)
+                    .success(null)
+                    .error(null)
+                    .mapTo(null)
+                    .success(null)
+                    .error(null)
+            ;
+            defer.complete(31);
+        }
+        System.out.println(watch.end().elapsed() + " total: " + PromiseImpl.total);
+    }
+
+    public static void test7() {
+        Watch watch = new Watch().start();
+        for (int i = 0; i < 10_000_000; i++) {
+            final Defer<Object> defer = defer();
+            defer.promise()
+                    .success(null)
+                    .error(null)
+                    .mapTo(s -> s.toString())
+                    .mapTo(s -> s.split("-"))
+                    .success(null)
+                    .mapTo(s -> String.join("-", s))
+                    .complete(null)
+                    .error(null)
+                    .mapToPromise(m -> Promises.success(m.split("5")))
+                    .complete(null)
+                    .mapTo(s -> String.join("-", s))
+                    .complete(null)
+                    .error(null)
+                    .mapToPromise(m -> Promises.success(m.split("5")))
+                    .complete(null)
+                    .mapTo(s -> s.length)
+                    .success(null)
+                    .error(null)
+                    .mapTo(null)
+                    .mapToPromise(null)
+                    .complete(null)
+                    .mapToVoid(null)
+                    .error(null)
+                    .mapTo(null)
+                    .mapToPromise(null)
+                    .complete(null)
+                    .mapToVoid(null)
+                    .success(null)
+                    .error(null)
+                    .mapTo(v -> {
+                        return "[void]";
+                    })
+                    .success(null)
+                    .error(null)
+            ;
+            defer.complete(123);
+        }
+        System.out.println(watch.end().elapsed() + " total: " + PromiseImpl.total);
+    }
+
+    public static void test6() {
+        final Defer<Object> defer = defer();
+        defer.promise()
+                .success(null)
+                .error(null)
+                .success(null)
+                .complete(null)
+                .error(null)
+                .complete(null)
+                .success(null)
+                .error(null)
+        ;
+        defer.fail(new Exception("NO NO"));
+    }
+
+    public static void test5() {
+        final Defer<Object> defer = defer();
+        defer.promise()
+                .success(null)
+                .error(null)
+                .success(null)
+                .complete(null)
+                .error(null)
+                .complete(null)
+                .success(null)
+                .error(null)
+        ;
+        defer.complete();
+    }
+
+    public static void test4() {
+        final Defer<Object> defer = defer();
+        defer.promise()
+                .success(null)
+                .error(null)
+                .success(null)
+                .complete(null)
+                .error(null)
+                .complete(null)
+                .success(null)
+                .error(null)
+        ;
+        defer.complete("8888888888888888888");
+    }
+
+    public static void test3() {
+        final Defer<Object> defer = defer();
+        defer.promise()
+                .success(null)
+                .error(null)
+                .success(null)
+                .complete(null)
+                .error(null)
+                .complete(null)
+                .success(null)
+                .error(null)
+        ;
+        defer.fail(new TimeoutException("Delay: 55"));
+    }
+
+    private static void test2() {
+        error(new TimeoutException("Timed out: 55"))
+                .success(null)
+                .error(null)
+                .success(null)
+                .complete(null)
+                .error(null)
+                .complete(null)
+                .success(null)
+                .error(null)
+        ;
+    }
+
+    private static void test1() {
+        success("Sona")
+                .success(null)
+                .error(null)
+                .success(null)
+                .complete(null)
+                .error(null)
+                .complete(null)
+                .success(null)
+                .error(null)
+        ;
+    }
+}
